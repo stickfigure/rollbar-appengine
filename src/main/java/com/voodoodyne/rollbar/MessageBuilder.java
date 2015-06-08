a3927a6b80fe467e14e9b564397c12477f303969
Jeff Schnitzer <jeff@infohazard.org>
2015-06-07 23:33:21 -0700

    Limit stack frames to 500
    Prevents hitting the 100kb GAE task size limit when a
    StackOverflowError happens.

--- src/main/java/com/voodoodyne/rollbar/MessageBuilder.java
+++ src/main/java/com/voodoodyne/rollbar/MessageBuilder.java
@@ -1,15 +1,19 @@
 package com.voodoodyne.rollbar;
 
 import com.tapstream.rollbar.NotifyBuilder;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import javax.inject.Inject;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 /**
  *
  */
 public class MessageBuilder {
+	/** Arbitrarily decide that 500 stack frames is the limit */
+	private static final int MAX_STACK_FRAMES = 500;
 
 	public static final String NOTIFIER_VERSION = "1.0";
 
@@ -22,7 +26,8 @@ public class MessageBuilder {
 
 	/**
 	 * Build a JSON string that should be sent off to rollbar. We delegate to the wrapped notifierbuilder,
-	 * then override some values.
+	 * then override some values. We also need to prevent the string from exceeding 100kb, the max
+	 * size of a task queue task. Mostly this is a concern for StackOverflowErrors.
 	 */
 	public String buildJson(String level, String message, Throwable throwable, Map<String, String> context) {
 		final JSONObject json = notifyBuilder.build(level, message, throwable, context);
@@ -34,6 +39,33 @@ public class MessageBuilder {
 		notifier.put("version", NOTIFIER_VERSION);
 		json.put("notifier", notifier);
 
+		truncateExcessiveStackFrames(json);
+
 		return json.toString();
+	}
+
+	/**
+	 * Recursively look for 'frames' and truncate any beyond an unreasonable #.
+	 * This JSON library is ghastly.
+	 */
+	private void truncateExcessiveStackFrames(Object json) {
+		if (json instanceof JSONObject) {
+			for (String key : (Set<String>)((JSONObject)json).keySet()) {
+				if (key.equals("frames")) {
+					JSONArray framesArray = ((JSONObject)json).optJSONArray(key);
+					if (framesArray != null) {
+						for (int i = framesArray.length(); i > MAX_STACK_FRAMES; i--) {
+							framesArray.remove(i-1);
+						}
+					}
+				} else {
+					truncateExcessiveStackFrames(((JSONObject)json).get(key));
+				}
+			}
+		} else if (json instanceof JSONArray) {
+			for (int i = 0; i < ((JSONArray)json).length(); i++) {
+				truncateExcessiveStackFrames(((JSONArray)json).get(i));
+			}
+		}
 	}
 }
